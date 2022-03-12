9d57e233cd5e311ce768f53c5b783cdd44e74995
SuHyeonOk <tntngus1223@naver.com>
2022-03-12 23:12:12 +0900

    10171번 고양이 출력하기 (역슬래시 해결!)

---
+++ BaekJoon/10171.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	std::cout << "\\    /\\" << std::endl;
+	std::cout << " )  ( ')" << std::endl;
+	std::cout << "(  /  )" << std::endl;
+	std::cout << " \\(__)|" << std::endl;
+	return 0;
+}
