3e1dabe7cb42fb4892325790f0b73411e92c27cf
SuHyeonOk <tntngus1223@naver.com>
2022-03-13 23:50:51 +0900

    10172번 강아지 출력 \\와 "" 문제 해결

---
+++ BaekJoon/10172.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	std::cout << "|\\_/|" << std::endl;
+	std::cout << "|q p|   /}" << std::endl;
+	std::cout << "( 0 )\"\"\"\\" << std::endl;
+	std::cout << "|\"^\"`    |" << std::endl;
+	std::cout << "||_/=\\\\__|" << std::endl;
+	return 0;
+}
