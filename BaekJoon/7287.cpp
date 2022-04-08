5017d612edecdcb7bcf347d019b34d8023511a83
SuHyeonOk <tntngus1223@naver.com>
2022-04-08 20:18:14 +0900

    7287번 등록 그냥 진짜 맞은 개수와 아이디를 넣어주면 된다

---
+++ BaekJoon/7287.cpp
@@ -0,0 +1,20 @@
+#pragma once // 헤더의 중복 선언을 방지
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#include <string>
+#include <cstring>
+#define endl '\n'
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	// ◆ 7287번 등록
+	cout << "100" << endl; //실제 맞은 갯수
+	cout << "totoro05" << endl; //실제 아이디
+
+	return 0;
+}
