c70bcc7d7f3d66027e33778a9d893d530b186b91
SuHyeonOk <tntngus1223@naver.com>
2022-04-12 22:35:05 +0900

    5338번 마이크로소프트 로고

---
+++ BaekJoon/5338.cpp
@@ -0,0 +1,23 @@
+#pragma once // 헤더의 중복 선언을 방지
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#include <string>
+#include <cstring>
+#define endl '\n'
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	// ◆ 5338번 마이크로소프트 로고
+	cout << "       _.-;;-._" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-;;-._|" << endl;
+	cout << "'-..-'|   ||   |" << endl;
+	cout << "'-..-'|_.-''-._|" << endl;
+
+	return 0;
+}
