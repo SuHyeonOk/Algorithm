9a3813bd7d2c42df29683c1c5c25d374046ca0d8
SuHyeonOk <tntngus1223@naver.com>
2022-04-16 18:18:18 +0900

    11721번 열 개씩 끊어 출력하기 이 문제는 파이썬으로 푸는 것이 현명하다고 한다 C++에서는 지원하지 않는 기능이기에 이를 구현하려면 코드가 많이 길어진다

---
+++ BaekJoon/11721.cpp
@@ -0,0 +1,23 @@
+#pragma once // 헤더의 중복 선언을 방지
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+#include <string>
+#include <cstring>
+#define endl '\n'
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
+	// ◆ 11721번 열 개씩 끊어 출력하기
+	// 이 문제는 파이썬으로 푸는 것이 현명하다고 한다
+
+	a = int(input())
+		b = int(input())
+		print(a + b)
+		print(a - b)
+		print(a * b)
+}
