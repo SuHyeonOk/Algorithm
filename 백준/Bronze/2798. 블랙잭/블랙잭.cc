b7b0a4c89bcf6afee7822f90bd10869bb511e7a5
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-05-09 12:46:40 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 
 using namespace std;
 
 int n{ 0 }, m{ 0 }, result{ 0 };
+int arrNum[100]{ 0 };
 bool	visited[100]{ false };
-vector<int> vec;
 
-void	Dfs(int num, int sum)
+void	Dfs(int depth, int sum)
 {
-	if (3 == num)
+	if (3 == depth)
 	{
 		if (m >= sum)
 			result = max(result, sum);
-
+		
 		return;
 	}
 
 	for (size_t i = 0; i < n; i++)
 	{
-		if (false != visited[i]) return;
+		if (true == visited[i]) return;
 
 		visited[i] = true;
-		Dfs(num + 1, sum + vec[i]);
+		Dfs(depth + 1, sum + arrNum[i]);
 		visited[i] = false;
 	}
 }
@@ -31,16 +30,11 @@ void	Dfs(int num, int sum)
 int main()
 {
 	// 2798
-	// n : 3~100 m : 10~300000
 
 	cin >> n >> m;
 
-	for (size_t i = 0; i < n; i++)
-	{
-		int Num{ 0 };
-		cin >> Num;
-		vec.push_back(Num);
-	}
+	for (int i = 0; i < n; ++i)
+		cin >> arrNum[i];
 
 	Dfs(0, 0);
 
