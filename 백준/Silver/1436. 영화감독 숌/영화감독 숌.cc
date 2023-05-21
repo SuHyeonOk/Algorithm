264637af333608fd93fb8eef00218a6bf1e5450f
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-05-21 15:00:27 +0900

    [Silver V] Title: 영화감독 숌, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,31 +1,32 @@
 #include<iostream>
-using namespace std;
 
-int N, ans, cnt, temp;
+	using namespace std;
 
-int main()
-{
-	
-	cin >> N;
-
-	ans = 0; // 영화 제목
-	cnt = 0; // 현재 몇번쨰 종말의 수인지
-
-	while (cnt != N)
+	int main()
 	{
-		ans++;
-		temp = ans;
+		// 1436 : 영화감독 숌
+		
+		int n{ 0 }, ans{ 0 }, count{ 0 }, temp{ 0 };
+		cin >> n;
 
-		// 수에 6이 적어도 3개이상 들어가는지 판별
-		while (temp != 0)
+		while (count != n)
 		{
-			if (temp % 1000 == 666) // 종말의 숫자라면
+			ans++;
+			temp = ans;
+
+			// 수에 6이 적어도 3개이상 들어가는지 판별
+			while (temp != 0)
 			{
-				cnt++;
-				break;
+				if ((temp % 1000) == 666) // 종말의 숫자
+				{
+					count++;
+					break;
+				}
+				else
+				{
+					temp /= 10; // 일의 자리수 삭제
+				}
 			}
-			else temp /= 10; // 일의 자리수 삭제
 		}
+		cout << ans;
 	}
-	cout << ans;
-}
