f39319d410aa0ccd3f515679c4fa2d65fe1882a3
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-05-26 14:36:12 +0900

    [Silver IV] Title: 카드2, Time: 8 ms, Memory: 4156 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.cc
+++ 백준/Silver/2164. 카드2/카드2.cc
@@ -6,7 +6,7 @@ int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(nullptr);
 
-    int n{ 0 }, temp{ 0 };
+    int n{ 0 };
     cin >> n;
     
     queue<int> que;
@@ -15,21 +15,11 @@ int main()
         que.push(i);
     }
 
-    bool b{ false };
-    while (que.size() != 1)
+    while (que.size() > 1)
     {
-        if (b == false)
-        {
-            que.pop();
-            b = true;
-        }
-        else
-        {
-            temp = que.front();
-            que.pop();
-            que.push(temp);
-            b = false;
-        }
+        que.pop();
+        que.push(que.front());
+        que.pop();
     }
 
     cout << que.front() << '\n';
