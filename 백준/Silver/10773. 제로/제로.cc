12784a9e4ef0c43d1d759b5b17d744c7a8b4b8f6
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-05-29 21:10:04 +0900

    [Silver IV] Title: 제로, Time: 8 ms, Memory: 2548 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cc
+++ 백준/Silver/10773. 제로/제로.cc
@@ -3,43 +3,24 @@
 
 using namespace std;
 
-int main() {
-
-
+int main() 
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int K{ 0 }, temp{ 0 }; // 1 <= K <= 100,000
+    int K{ 0 }, temp{ 0 }, result{ 0 }; // 1 <= K <= 100,000
     stack<int> sta; // 0 : pop / 나머지 : push
     
     cin >> K;
     for (int i = 0; i < K; i++)
     {
         cin >> temp;
-        if (0 == temp)
-        {
-            sta.pop();
-        }
-        else
-        {
-            sta.push(temp);
-        }
+        if (0 == temp) { result -= sta.top(); sta.pop(); }
+        else { result += temp; sta.push(temp); }
     }
 
-    if (sta.empty()) 
-    {
-        cout << "0" << "\n"; 
-        return 0;
-    }
-
-    temp = 0;
-    while (!sta.empty())
-    {
-        temp += sta.top();
-        sta.pop();
-    }
-
-    cout << temp << "\n";
+    if (sta.empty()) { cout << "0" << "\n"; return 0; }
+    cout << result << "\n";
 
     return 0;
 }
