4438dadb1885c55833fce783da3ca915824cb6ba
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-06-03 23:03:38 +0900

    [Bronze V] Title: 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+    int n;
+    string k;
+    cin >> n;
+    while (n--)
+    {
+        cin >> k;
+        cout << k[0] << k[k.size() - 1] <<'\n';
+    }
+}
