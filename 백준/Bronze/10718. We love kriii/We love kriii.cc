fa08a626f360b8a7fb731535f91abd71204e291e
SuHyeonOk <77478337+SuHyeonOk@users.noreply.github.com>
2023-08-04 16:55:30 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	std::ios_base::sync_with_stdio(false); 
+	std::cin.tie(NULL);
+
+	std::cout << "강한친구 대한육군" << std::endl;
+	std::cout << "강한친구 대한육군" << std::endl;
+	return 0;
+}
